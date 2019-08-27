533982a4e2c361138921540f235f0a13570f93c5
Sokabi <mosokabi@9801a7895d93.ant.amazon.com>
2019-08-27 21:05:31 +0100

    Image Classifer

--- w1/index.js
+++ w1/index.js
@@ -1,90 +1,36 @@
 let net;
 
-const webcamElement = document.getElementById("webcam");
-const classifier = knnClassifier.create();
 
 async function app() {
-    console.log('Loading mobilenet..');
-  
-    // Load the model.
-    net = await mobilenet.load();
-    console.log('Sucessfully loaded model');
-  
-    await setupWebcam();
-  
-    // Reads an image from the webcam and associates it with a specific class
-    // index.
-    const addExample = classId => {
-      // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-      // to the KNN classifier.
-      const activation = net.infer(webcamElement, 'conv_preds');
-  
-      // Pass the intermediate activation to the classifier.
-      classifier.addExample(activation, classId);
-    };
-  
-    // When clicking a button, add an example for that class.
-    document.getElementById('class-a').addEventListener('click', () => addExample(0));
-    document.getElementById('class-b').addEventListener('click', () => addExample(1));
-    document.getElementById('class-c').addEventListener('click', () => addExample(2));
-  
-    while (true) {
-      if (classifier.getNumClasses() > 0) {
-        // Get the activation from mobilenet from the webcam.
-        const activation = net.infer(webcamElement, 'conv_preds');
-        // Get the most likely class and confidences from the classifier module.
-        const result = await classifier.predictClass(activation);
-  
-        const classes = ['A', 'B', 'C'];
-        document.getElementById('console').innerText = `
-          prediction: ${classes[result.classIndex]}\n
-          probability: ${result.confidences[result.classIndex]}
-        `;
-      }
-  
-      await tf.nextFrame();
-    }
+  console.log('Loading mobilenet..');
+  net = await mobilenet.load();
+  // Load the model.
+  console.log('Sucessfully loaded model');
+  document.getElementById("predictButton").onclick = async function () {
+    var imgEl = document.getElementById("blah");
+    const classifier = await net.classify(imgEl);
+    console.log(classifier);
+    var resultClass = document.getElementById("result");
+    resultClass.innerHTML = classifier[0]
+
   }
+}
 
-async function app() {
-    console.log('Loading mobilenet..');
 
-    // Load the model.
-    net = await mobilenet.load();
-    console.log('Sucessfully loaded model');
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-    await setupWebcam();
-    while (true) {
-        const result = await net.classify(webcamElement);
-
-        document.getElementById('console').innerText = `
-        prediction: ${result[0].className}\n
-        probability: ${result[0].probability}
-      `;
-
-        // Give some breathing room by waiting for the next animation frame to
-        // fire.
-        await tf.nextFrame();
+    reader.onload = function (e) {
+      var imgEl = document.getElementById("blah");
+      var newImage = e.target.result;
+      imgEl.setAttribute("src", newImage);
     }
+
+    reader.readAsDataURL(input.files[0]);
+  }
 }
 
-async function setupWebcam() {
-    return new Promise((resolve, reject) => {
-        const navigatorAny = navigator;
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-            navigatorAny.msGetUserMedia;
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({ video: true },
-                stream => {
-                    webcamElement.srcObject = stream;
-                    webcamElement.addEventListener('loadeddata', () => resolve(), false);
-                },
-                error => reject());
-        } else {
-            reject();
-        }
-    });
-}
+
 
 app();
